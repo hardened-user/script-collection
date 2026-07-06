72ce341d9f9c12d777ffe57f5d09f0712e05f613
Aleksey K. <aleksey.adm@gmail.com>
2026-07-06 15:43:50 +0300

    +

--- k8s/hprint_get_ingress.py
+++ k8s/hprint_get_ingress.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # ----------------------------------------------------------------------------------------------------------------------
-# Print Ingress in human-readable format
-# Example:
-#   $ kubectl get ingress --all-namespaces -o yaml | ./hprint_get_ingress.py
-# ----------------------------------------------------------------------------------------------------------------------
+import argparse
 import sys
 import traceback
 
@@ -12,9 +9,22 @@ from ruamel.yaml import YAML
 
 
 def main():
+    # __________________________________________________________________________
+    # command-line options, arguments
+    try:
+        parser = argparse.ArgumentParser(
+            description='Print Kubernetes Ingress resources in human-readable table format.',
+            epilog='example:\n\tkubectl get ingress --all-namespaces -o yaml | %(prog)s',
+            formatter_class=argparse.RawDescriptionHelpFormatter
+        )
+        # noinspection PyUnusedLocal
+        args = parser.parse_args()  # <class 'argparse.Namespace'>
+    except SystemExit:
+        return False
+    # __________________________________________________________________________
     yaml = YAML()
     yaml.indent(mapping=2, sequence=4, offset=2)
-    buffer = dict()
+    buffer: dict[str, dict[str, dict[str, list[str]]]] = dict()
     ljust = {
         'ns': 0,
         'name': 0,
@@ -49,17 +59,25 @@ def main():
                 buffer[ns][name][host].append(path)
                 if len(path) > ljust.get('path', 0):
                     ljust['path'] = len(path)
-    # ------------------------------------------------------------------------------------------------------------------
+    # __________________________________________________________________________
+    headers = {'ns': 'NAMESPACE', 'name': 'NAME', 'host': 'HOST', 'path': 'PATH'}
+    for key in ljust:
+        ljust[key] = max(ljust[key], len(headers[key]))
+    fmt = "| {} | {} | {} | {} |"
+    print(fmt.format(*(headers[k].ljust(ljust[k]) for k in ljust)), flush=True)
+    print(fmt.format(*('-' * ljust[k] for k in ljust)), flush=True)
+    prev_ns = prev_name = prev_host = ""
     for ns in sorted(buffer):
-        for idx_name, name in enumerate(sorted(buffer[ns])):
-            for idx_host, host in enumerate(sorted(buffer[ns][name])):
-                for idx_path, path in enumerate(sorted(buffer[ns][name][host])):
-                    print("| {} | {} | {} | {} |".format(
-                        ns.ljust(ljust['ns']) if idx_name == 0 and idx_host == 0 and idx_path == 0 else " " * ljust['ns'],
-                        name.ljust(ljust['name']) if idx_host == 0 and idx_path == 0 else " " * ljust['name'],
-                        host.ljust(ljust['host']) if idx_path == 0 else " " * ljust['host'],
+        for name in sorted(buffer[ns]):
+            for host in sorted(buffer[ns][name]):
+                for path in sorted(buffer[ns][name][host]):
+                    print(fmt.format(
+                        ns.ljust(ljust['ns']) if ns != prev_ns else ' ' * ljust['ns'],
+                        name.ljust(ljust['name']) if (ns, name) != (prev_ns, prev_name) else ' ' * ljust['name'],
+                        host.ljust(ljust['host']) if (ns, name, host) != (prev_ns, prev_name, prev_host) else ' ' * ljust['host'],
                         path.ljust(ljust['path'])))
-    # ------------------------------------------------------------------------------------------------------------------
+                    prev_ns, prev_name, prev_host = ns, name, host
+    # __________________________________________________________________________
     return True
 
 
